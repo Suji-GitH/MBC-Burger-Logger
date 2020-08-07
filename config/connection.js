6869bf3dea16b17725d1bbb3ec0234f38a73ec5f
Sujith Ranjitkumar <sujithranjitk@gmail.com>
2020-08-07 14:25:20 +1000

    update connection

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,19 @@ const dotevn = require("dotenv").config();
 //Require npm my sql
 const mysql = require("mysql");
 
-//Connection config for DB
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME
+    });
+};
 
 //Connect to DB
 connection.connect(err => {
@@ -21,5 +26,6 @@ connection.connect(err => {
     console.log(`Connection Id: ${connection.threadId}`);
 });
 
+connection.connect();
 //Export connection for our ORM to use.
 module.exports = connection;
